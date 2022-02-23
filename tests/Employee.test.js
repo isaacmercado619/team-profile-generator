7fe221125e03dce2b272cf8e4a5effc47abbaddb
Isaac <mercado.isaac619@gmail.com>
2022-02-22 17:11:49 -0800

    fixed the 4th test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,5 +1,12 @@
 const Employee = require('../lib/employee');
 
 describe('Employee', () => {
-    
+    describe('Initialization', () => {
+         it ("should create an object with a name, id and email.", () => {
+             const employee = new Employee("Mike", 1, "you@gmail.com");
+             expect(employee.name).toEqual("Mike");
+             expect(employee.id).toEqual(1);
+             expect(employee.email).toEqual("you@gmail.com");
+         })
+    })
 })
